76c985ba7c6dc2c65c54921daf876e835fac7477
Pradeep Detroja <detrojapradeep111@gmail.com>
2024-06-04 00:20:10 +0100

    index.js working for clicks

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,42 @@
+var allButtons = document.querySelectorAll(".drum");
+for (var i = 0; i < allButtons.length; i++) {
+    allButtons[i].addEventListener("click", function() {
+        var buttonInnerHTML = this.innerHTML;
+        
+        switch(buttonInnerHTML) {
+            case 'w':
+                var audio1 = new Audio("./sounds/crash.mp3");
+                audio1.play();
+                break;
+            case 'a':
+                var audio1 = new Audio("./sounds/kick-bass.mp3");
+                audio1.play();
+                break;
+            case 's':
+                var audio1 = new Audio("./sounds/snare.mp3");
+                audio1.play();
+                break;
+            case 'd':
+                var audio1 = new Audio("./sounds/tom-1.mp3");
+                audio1.play();
+                break;
+            case 'j':
+                var audio1 = new Audio("./sounds/tom-2.mp3");
+                audio1.play();
+                break;
+            case 'k':
+                var audio1 = new Audio("./sounds/tom-3.mp3");
+                audio1.play();
+                break;
+            case 'l':
+                var audio1 = new Audio("./sounds/tom-4.mp3");
+                audio1.play();
+                break;
+
+            default:
+                console.log("Switch case gone wrong");
+                break;
+        }
+    });
+}
+
